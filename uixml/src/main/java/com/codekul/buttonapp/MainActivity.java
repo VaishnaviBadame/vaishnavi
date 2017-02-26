4b0f1e157bca117509d227e8c5febf9d22c38460
vaishnavi2996 <vaishnavibadame2996@gmail.com>
2017-02-26 13:05:57 +0530

    initial commit

---
+++ uixml/src/main/java/com/codekul/buttonapp/MainActivity.java
@@ -0,0 +1,57 @@
+package com.codekul.buttonapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+        public static final String TAG = MainActivity.class.getCanonicalName();
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            Button btnOkay = (Button) findViewById(R.id.btnOkay);
+              /*btnOkay.setOnClickListener(new Click());
+                btnOkay.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    EditText editName = (EditText) findViewById(R.id.editName);
+                    String data = editName.getText().toString();
+                    editName.setText(data.toUpperCase());
+                }
+            });
+
+              btnOkay.setOnClickListener( view -> {
+                makeUpperCase(view);
+        });*/
+
+            btnOkay.setOnClickListener(this::makeUpperCase);
+        }
+
+        private void makeUpperCase(View view) {
+            EditText edtAnyName = (EditText) findViewById(R.id.editName);
+            String data = edtAnyName.getText().toString();
+            edtAnyName.setText(data.toUpperCase());
+        }
+
+        private class Click implements View.OnClickListener {
+            @Override
+            public void onClick(View v) {
+
+                EditText edtAnyName = (EditText) findViewById(R.id.editName);
+                String data = edtAnyName.getText().toString();
+                edtAnyName.setText(data.toUpperCase());
+
+                Log.i(TAG, data);
+            }
+        }
+    }
+
